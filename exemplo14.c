7250902ecca612eb6f4e1ad314ca50250b87d74f
aldo.santos.oliveira95@gmail.com <aldo.santos.oliveira95@gmail.com>
2021-09-26 17:30:16 -0300

    Como Inverter um número | Operador Resto %

---
+++ exemplo14.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+int numero, cifra;
+
+printf("Digite um numero inteiro:\n");
+scanf("%i", &numero);
+
+if (numero >= 0){
+do {
+
+cifra = numero % 10;
+printf("%i", cifra);
+numnero /= 10;
+}
+while (numero != 0);
+printf("\n");
+}
+
+return 0;
+
+}
