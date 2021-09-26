41e7fe5e5d9a72227a0c0fc5361ff0c99fd913f1
aldo.santos.oliveira95@gmail.com <aldo.santos.oliveira95@gmail.com>
2021-09-26 19:40:53 -0300

    Como utilizar Operador Condicional ?

---
+++ exemplo15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int num1 = 10;
+int num2 = 20;
+
+int resposta;
+
+num1 < num2 ? printf("Sim\n") : printf("nao\n");
+
+printf("%in", resposta);
+
+return 0;
+
+}
