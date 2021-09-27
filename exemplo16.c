e0dfd2e9bd1fc0778f448cedbfbd25d359819eac
aldo.santos.oliveira95@gmail.com <aldo.santos.oliveira95@gmail.com>
2021-09-27 12:41:06 -0300

    Revisão Tipos de Variáveis

---
+++ exemplo16.c
@@ -0,0 +1,41 @@
+#inlude <stdio.h>
+
+int main(){
+
+char variavelChar = 'A';
+printf("%c\n", variavelChar);
+scanf("%c", &variavelChar);
+printf("%c\n", variavelChar);
+
+/*
+bool variavelBool = true;
+printf("%i\n", variavelBool);
+*/
+
+/* int variavelInt = 10;
+printf("%i\n", variavelInt);
+scanf("%", &variavelInt);
+printf("%i\n", variavelInt);
+*/
+
+
+/*float variavelFloat = 10.10;
+printf("%f\n", variavelFloat);
+scanf("%f", &variavelFloat);
+printf("%f", variavelFloat);
+*/
+
+/*double variavelDouble = 10.10;
+printf("%f\n", variavelDouble);
+scanf("%lf", &variavelDouble);
+printf("%f", variavelDouble);
+*/
+
+return 0;
+
+}
+
+
+
+
+
